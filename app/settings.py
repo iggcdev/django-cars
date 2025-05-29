01c08cee0225da4ad8f7a0dba86a1ed337688ad6
= <=>
2025-05-29 10:40:19 -0300

    Config: Allowed Hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8y3(btte=16if^g7c-utu84dzyqjx^g8vu75@eb@2yk===ax7+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
